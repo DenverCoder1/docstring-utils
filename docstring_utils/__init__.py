6579f98f9150dcc3a2a3d1d0b8d50f9c3101d71e
Jonah Lawrence <jonah@freshidea.com>
2022-09-06 14:29:29 -0600

    chore: bump version to 1.0.0 (#7)

--- docstring_utils/__init__.py
+++ docstring_utils/__init__.py
@@ -5,7 +5,7 @@ docstring-utils: Parser for Numpy, Sphinx, and Google-style docstrings
 from .parsed_docstring import ParsedDocstring, ParsedArgument, ParsedReturnValue
 from .parser import parse_docstring
 
-__version__ = "0.0.1"
+__version__ = "1.0.0"
 
 __all__ = [
     "__version__",
